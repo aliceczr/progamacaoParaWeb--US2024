e2ff9953544c2824be92d576671b13a506a7a45e
Alice Cruz <127883364+aliceczr@users.noreply.github.com>
2024-04-29 09:47:04 -0300

    Update app.js

--- atividade5/app.js
+++ atividade5/app.js
@@ -3,22 +3,22 @@ const app = express();
 
 
 
-// Simulação de um estoque de produtos
+
 let estoque = [];
 
-// Rota para adicionar um novo produto ao estoque
+
 app.get('/adicionar/:id/:nome/:qtd', (req, res) => {
     const { id, nome, qtd } = req.params;
     estoque.push({ id, nome, quantidade: parseInt(qtd) });
     res.send('Produto adicionado ao estoque com sucesso.');
 });
 
-// Rota para listar todos os produtos do estoque
+
 app.get('/listar', (req, res) => {
     res.json(estoque);
 });
 
-// Rota para remover um produto do estoque
+
 
 app.get('/remover/:id', (req, res) => {
     const { id } = req.params;
@@ -26,7 +26,7 @@ app.get('/remover/:id', (req, res) => {
     res.send('Produto removido do estoque com sucesso.');
 });
 
-// Rota para editar a quantidade de um produto no estoque
+
 app.get('/editar/:id/:qtd', (req, res) => {
     const { id, qtd } = req.params;
     const index = estoque.findIndex(produto => produto.id === id);
@@ -38,7 +38,7 @@ app.get('/editar/:id/:qtd', (req, res) => {
     }
 });
 
-// Iniciar o servidor
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
